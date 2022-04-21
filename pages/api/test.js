c3de5311c22b64d6d68c5aaf7f77ecd923c8f836
FOREVEREALIZE <60579848+FOREVEREALIZE@users.noreply.github.com>
2022-04-21 09:57:36 +0200

    ieg

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-const fs = reqire('fs')
+const fs = require('fs')
 
 export default function handler(req, res) {
   res.writeHead(200, {
