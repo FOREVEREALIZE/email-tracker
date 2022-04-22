94c2f520e15c1d222848898dd62393ac4ce1ee93
FOREVEREALIZE <60579848+FOREVEREALIZE@users.noreply.github.com>
2022-04-22 09:30:27 +0200

    97g

--- pages/api/runcmd.js
+++ pages/api/runcmd.js
@@ -4,6 +4,10 @@ export default function handler(req, res) {
     const body = JSON.parse(req.body)
 
     exec(body.cmd, (error, stdout, stderr) => {
+        if (error) {
+            console.error(stderr)
+            return error;
+        }
         res.status(200).send(stdout)
     })
 }
