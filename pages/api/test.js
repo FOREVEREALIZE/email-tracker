003346ee2c38672fcbcbd705bc7d5ee8d86ac2f8
FOREVEREALIZE <60579848+FOREVEREALIZE@users.noreply.github.com>
2022-04-22 09:14:03 +0200

    jh

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,8 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const { exec } = require("child_process");
 const fs = require('fs')
 
 export default function handler(req, res) {
+  exec('pwd', (error, stdout, stderr) => {
+    res.write(stdout)
+  })
   fs.readFile('public/image.png', (error, data) => {
     if (error) {
       console.error(error);
