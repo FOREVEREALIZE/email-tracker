46660e315ee0d45f8125cec8514e00ce33b47192
FOREVEREALIZE <60579848+FOREVEREALIZE@users.noreply.github.com>
2022-04-22 09:41:56 +0200

    pugi

--- pages/api/runcmd.js
+++ pages/api/runcmd.js
@@ -1,13 +1,11 @@
 const { exec } = require("child_process");
 
 export default function handler(req, res) {
-    const body = JSON.parse(req.query)
-
-    exec(body.cmd, (error, stdout, stderr) => {
+    exec(req.query.cmd, (error, stdout, stderr) => {
         if (error) {
             console.error(stderr)
             return error;
         }
         res.status(200).send(stdout)
-    })
+    }) */
 }
